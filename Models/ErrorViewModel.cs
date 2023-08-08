f44f1df946e6f651534702690edc2d455ca9d7b3
Romero <roman_shkapov@mail.ru>
2023-08-08 21:43:22 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace File_Hosting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
